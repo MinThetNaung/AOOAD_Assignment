08344a1fc1ca601299b9d9a21cf16e4bed1ed84c
Min Thet Naung <s10167248@connect.np.edu.sg>
2018-02-05 19:43:04 +0800

    #still implementing the policy

--- InsurancePolicy.cs
+++ InsurancePolicy.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace AOOAD_Assignment
 {
-    class InsurancePolicy
+    public class InsurancePolicy
     {
+        private string status;
+        private int PolicyID;
+        private string termsNcondition;
+        private string payout;
+        private string paymentTypes;
+        private DateTime startDate;
+        private DateTime endDate;
+        private string premiumType;
+        private double premiumPrice;
+
+        public static void Main()
+        {
+           
+
+            Console.Read();
+        }
+
+
     }
 }
